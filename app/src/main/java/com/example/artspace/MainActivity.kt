c70419472d2b85166a67fd705d13ab5cfc25eff9
Vandyka Suryadi <vandykasuryadi@gmail.com>
2026-08-07 21:14:43 +0700

    feat: menambahkan gambar dan spacing antara deskripsi

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,6 +49,12 @@ fun ArtSpaceApp() {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(R.drawable.petra_jordan),
+                contentDescription = stringResource(R.string.petra),
+                modifier = Modifier.padding(horizontal = 36.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
             DescriptionCard()
         }
     }
@@ -52,7 +62,9 @@ fun ArtSpaceApp() {
 
 @Composable
 fun DescriptionCard() {
-    Column() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Description",
             fontWeight = FontWeight.Bold,
